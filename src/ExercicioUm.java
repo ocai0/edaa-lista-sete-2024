f372cd956bd68f7066961c272f3d316bdb0ebc3e
ocai0 <caioalvesfernandes@gmail.com>
2024-04-14 17:22:26 -0300

    Resolvido questão 1 c

--- src/ExercicioUm.java
+++ src/ExercicioUm.java
@@ -3,9 +3,12 @@ import java.util.Stack;
 class ExercicioUm {
     public static void resolve() {
         // ExercicioUm.resolveA("ESTE EXERCÍCIO É MUITO FÁCIL");
-        ExercicioUm.resolveB("Abba");
-        ExercicioUm.resolveB("ovo");
-        ExercicioUm.resolveB("ovos");
+        // ExercicioUm.resolveB("Abba");
+        // ExercicioUm.resolveB("ovo");
+        // ExercicioUm.resolveB("ovos");
+        int[] numbers = { 1, 4, 8, 5, 6, 3, 3, 2, 1, 9, 7 };
+        ExercicioUm.resolveC(numbers);
+
     }
 
     public static void resolveA(String originalWord) {
@@ -23,7 +26,17 @@ class ExercicioUm {
                         originalWord.substring(startB, endB)) ? "Verdadeiro" : "Falso");
     }
 
-    public static void resolveC() {
+    public static void resolveC(int[] numbers) {
+        StackOrdered stack = new StackOrdered();
+        stack.push(34);
+        stack.push(3);
+        stack.push(31);
+        stack.push(98);
+        stack.push(92);
+        stack.push(23);
+        while (!stack.empty()) {
+            System.out.println(stack.pop());
+        }
     }
 
     public static void resolveD() {
@@ -41,6 +54,58 @@ class ExercicioUm {
 
     public static boolean palindromeCheck(String wordA, String wordB) {
         return ExercicioUm.invertWord(wordB.toLowerCase()).equals(wordA.toLowerCase());
+    }
+
+}
+
+class StackOrdered {
+    Stack<Integer> stack;
+
+    public StackOrdered() {
+        this.stack = new Stack<Integer>();
+    }
+
+    public void push(int number) {
+        stack.add(number);
+        this.arrangeAscending();
+        // this.arrangeDescending();
+
+    }
+
+    private void arrangeDescending() {
+        Stack<Integer> tempStack = new Stack<Integer>();
+        int temp;
+        while (!stack.empty()) {
+            temp = stack.pop();
+            while (!tempStack.empty() && tempStack.lastElement() > temp)
+                stack.add(tempStack.pop());
+            tempStack.add(temp);
+        }
+        this.stack = tempStack;
+    }
+
+    private void arrangeAscending() {
+        Stack<Integer> tempStack = new Stack<Integer>();
+        int temp;
+        while (!stack.empty()) {
+            temp = stack.pop();
+            while (!tempStack.empty() && tempStack.firstElement() < temp)
+                stack.add(tempStack.pop());
+            tempStack.add(temp);
+        }
+        this.stack = tempStack;
+    }
+
+    public int pop() {
+        return this.stack.pop();
+    }
+
+    public int size() {
+        return stack.size();
+    }
+
+    public boolean empty() {
+        return stack.empty();
     }
 
 }
