b21e8040bb6857ef4634ed9b60af1f05ec0165a6
ocai0 <caioalvesfernandes@gmail.com>
2024-04-14 18:44:10 -0300

    Formatado saida ExercicioUm

--- src/ExercicioUm.java
+++ src/ExercicioUm.java
@@ -2,23 +2,29 @@ import java.util.Stack;
 
 class ExercicioUm {
     public static void resolve() {
-        // ExercicioUm.resolveA("ESTE EXERCÍCIO É MUITO FÁCIL");
-        // System.err.println("");
-        // ExercicioUm.resolveB("Abba");
-        // ExercicioUm.resolveB("ovo");
-        // ExercicioUm.resolveB("ovos");
-        // System.err.println("");
+        System.err.println("Resultado Exercício 1:");
+        System.err.println("--------------------------------------------------");
+        System.err.println("Letra a)");
+        ExercicioUm.resolveA("ESTE EXERCÍCIO É MUITO FÁCIL");
+        System.err.println("--------------------------------------------------");
+        System.err.println("Letra b)");
+        ExercicioUm.resolveB("Abba");
+        ExercicioUm.resolveB("ovo");
+        ExercicioUm.resolveB("ovos");
+        System.err.println("--------------------------------------------------");
+        System.err.println("Letra c)");
         int[] numbers = { 34, 3, 31, 98, 92, 23 };
-        // ExercicioUm.resolveC(numbers);
-        // System.err.println("");
+        ExercicioUm.resolveC(numbers);
+        System.err.println("--------------------------------------------------");
+        System.err.println("Letra d)");
         ExercicioUm.resolveD(numbers);
-        System.err.println("");
 
     }
 
     public static void resolveA(String originalWord) {
         for (String word : originalWord.split(" "))
             System.out.printf("%s ", ExercicioUm.invertWord(word));
+        System.out.println("");
     }
 
     public static void resolveB(String originalWord) {
